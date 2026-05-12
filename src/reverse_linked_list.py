b71f18dda1d77585ef1304ca70e7c1c89da393f3
João Barreto <Anothermailb@gmail.com>
2026-05-12 18:38:54 -0300

    Update reverse_linked_list.py

--- src/reverse_linked_list.py
+++ src/reverse_linked_list.py
@@ -1,17 +1,9 @@
-from src.my_node import MyNode
+from src.my_array import MyArray
 
 
-def reverse_linked_list(head: MyNode) -> MyNode:
-
-    previous = None
-    current = head
-
-    while current is not None:
-
-        next_node = current.next
-        current.next = previous
-
-        previous = current
-        current = next_node
-
-    return previous
+def two_sum(array: MyArray, target: int) -> tuple[int, int]:
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if array[i] + array[j] == target and i != j:
+                return (i, j)
+    return (-1, -1)
